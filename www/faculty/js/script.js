d23f5c2607c1f31bae5729a224f59b1c168eb3f9
Edward <52780080+edward1k22@users.noreply.github.com>
2019-07-13 18:36:29 +0300

    Add files via upload

---
+++ www/faculty/js/script.js
@@ -0,0 +1,9 @@
+menu.onclick = function MyFunction() {
+	var x = document.getElementById("myTopnav");
+
+	if(x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+}
